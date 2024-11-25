252619fb06749e9ee6c97b042c899142fe75df69
BESToloch2 <befanalex2@gmail.com>
2024-11-24 23:32:54 -0600

    solar system basic structure is ready

---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,11 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        BasicStructure bb=new BasicStructure();
+        bb.planets="2";
+        bb.moon="2";
+        bb.stars="10";
+        bb.sun="1";
+        System.out.println("solar system is ready");
+
+    }
+}
