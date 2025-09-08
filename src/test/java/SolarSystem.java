4e142508bd9dffdde62b1c937786f56b50d390c7
DesktopPC <befanalex2@gmail.com>
2025-09-07 21:33:18 -0500

    solar system basic structure is ready

---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,10 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        BasicStructure BB=new BasicStructure();
+        BB.Planets="2";
+        BB.Moon="2";
+        BB.Sun="1";
+        BB.Stars="10";
+        System.out.println("Solar system is ready!");
+    }
+}
